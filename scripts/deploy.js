d0bdaaa819c4a57e7c4626342cde38939b1efcd1
João Tiago <jtiagodev@gmail.com>
2020-10-27 14:58:37 +0000

    setting up worker

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,20 +3,21 @@ const util = require("util");
 const fetch = require("node-fetch");
 const readFile = util.promisify(fs.readFile);
 const opn = require("opn");
+require('dotenv').config()
 
 async function deploy(script) {
   let resp = await fetch(
-    "https://api.cloudflare.com/client/v4/accounts" +
-      node.env.CLOUDFLARE_ACCOUNTID +
+    "https://api.cloudflare.com/client/v4/accounts/" +
+      process.env.CLOUDFLARE_ACCOUNTID +
       "/workers/scripts/" +
-      node.env.CLOUDFLARE_SCRIPT,
+      process.env.CLOUDFLARE_SCRIPT,
     {
       method: "PUT",
       headers: {
         "cache-control": "no-cache",
         "content-type": "text/javascript",
-        "X-Auth-Email": node.env.CLOUDFLARE_EMAIL,
-        "X-Auth-Key": node.env.CLOUDFLARE_KEY
+        "X-Auth-Email": process.env.CLOUDFLARE_EMAIL,
+        "X-Auth-Key": process.env.CLOUDFLARE_KEY
       },
       body: script
     }
